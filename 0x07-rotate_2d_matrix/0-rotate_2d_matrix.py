59987d6b2714c0322f95167011f89363e6cce6f7
serenalilian <serenalilian99@gmail.com>
2024-05-30 19:49:02 +0000

    2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+Rotate 2D Matrix
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    """
+    n = len(matrix)
+    for layer in range(n // 2):
+        first, last, offset = layer, n - 1 - layer, 0
+        for i in range(first, last):
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
+            offset += 1
+
+# def rotate_2d_matrix(matrix):
+#     n = len(matrix)
+
+#     # Transpose the matrix (swap rows with columns)
+#     for i in range(n):
+#         for j in range(i, n):
+#             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+#     # Reverse the rows to complete the rotation
+#     for i in range(n):
+#         matrix[i].reverse()
